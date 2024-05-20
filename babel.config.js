b289ad46fd6713f854c6bda9536d986525eefc8b
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-05-20 11:16:06 -0400

    Repro

---
+++ babel.config.js
@@ -0,0 +1,24 @@
+//babel.config.js
+
+const { buildEmberPlugins } = require('ember-cli-babel');
+
+module.exports = function (api) {
+  api.cache(true);
+
+  return {
+    presets: [
+      [
+        require.resolve('@babel/preset-env'),
+        {
+          targets: require('./config/targets'),
+        },
+      ],
+    ],
+    plugins: [
+      // this is where all the ember required plugins would reside
+      ...buildEmberPlugins(__dirname, {
+        /*customOptions if you want to pass in */
+      }),
+    ],
+  };
+};
